c874d6f7c742ca5e745c5a7ffc8b541be3c03ab7
FrancoLedesma <francoledesma100@gmail.com>
2023-12-12 16:45:28 -0300

    Initialized project and made a test spider

---
+++ src/index.js
@@ -0,0 +1,5 @@
+// const spider01 = require('./spiders/01-Example')
+const spider02 = require('./spiders/02-Pagination')
+
+// spider01()
+spider02()
---
+++ src/spiders/02-Pagination.js
@@ -0,0 +1,41 @@
+const puppeteer = require('puppeteer')
+
+const options = {
+  headless: false,
+  defaultViewport: false
+}
+
+async function example () {
+  const browser = await puppeteer.launch(options)
+  const page = await browser.newPage()
+
+  await page.goto('https://listado.mercadolibre.com.ar/guitarras#D[A:guitarras]')
+
+  try {
+    const data = await page.evaluate(() => {
+      const elements = document.querySelectorAll('.dynamic-carousel__link-container')
+
+      const extracted = Array.from(elements).map(e => {
+        const title = e.querySelector('.dynamic-carousel__title')
+        const price = e.querySelector('.dynamic-carousel__price span')
+        const image = e.querySelector('.dynamic-carousel__img')
+
+        return {
+          title: title ? title.textContent.trim() : '',
+          price: price ? price.textContent.trim() : '',
+          image: image ? image.getAttribute('src').trim() : ''
+        }
+      })
+
+      return extracted
+    })
+
+    console.log(data)
+  } catch (error) {
+    console.log(error)
+  }
+
+  await browser.close()
+}
+
+module.exports = example
